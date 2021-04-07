f082101e1a4ce786f1a3cf5690185ca7dc96b930
KoaLaYT <hytohyeah@outlook.com>
2021-04-08 00:06:47 +0800

    delete useless tests

--- cpp/test/winapi/window_api_test.cpp
+++ cpp/test/winapi/window_api_test.cpp
@@ -6,96 +6,35 @@
 #include <opencv2/opencv.hpp>
 
 #include <iostream>
-#include <io.h>
-#include <fcntl.h>
 #include <string>
 
-#define BASE "C:\\Users\\koalayt\\Desktop\\autozhuxian\\cpp\\"
+#define BASE "C:\\Users\\koalayt\\Desktop\\autozhuxian\\assets\\tmp\\"
 #define PATH(p) BASE p
 
-BITMAPINFOHEADER createBitmapHeader(int width, int height);
-cv::Mat captureScreenMat(HWND hwnd);
-
-static BOOL CALLBACK EnumWindowCb(HWND hwnd, LPARAM lParam)
+static void click()
 {
-    CHAR title[1024];
-    int length = GetWindowTextA(hwnd, title, sizeof(title));
+    using namespace std::chrono_literals;
 
-    std::cout << title << '\n';
+    INPUT Input{};
 
-    if (!IsWindowVisible(hwnd) ||
-        length == 0 ||
-        std::strcmp(title, "Program Manager") == 0 ||
-        std::strcmp("诛仙3", title) != 0) {
-        return TRUE;
+    Input.type = INPUT_MOUSE;
+    Input.mi.dx = 9000;  // desired X coordinate
+    Input.mi.dy = 9000;  // desired Y coordinate
+    Input.mi.dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE;
+    SendInput(1, &Input, sizeof(INPUT));
+
+    std::this_thread::sleep_for(1000ms);
+
+    Input.mi.dx = 0;
+    Input.mi.dy = 0;
+    for (int i = 0; i < 10; ++i) {
+        Input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+        SendInput(1, &Input, sizeof(INPUT));
+        Input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+        SendInput(1, &Input, sizeof(INPUT));
     }
-
-    SetForegroundWindow(hwnd);
-    cv::Mat screenshot = captureScreenMat(hwnd);
-    cv::imwrite(PATH("screenshot.png"), screenshot);
-
-    return TRUE;
 }
 
 TEST(windows_api, wchar)
 {
-    // _setmode(_fileno(stdout), _O_U16TEXT);
-    EnumWindows(EnumWindowCb, NULL);
-}
-
-BITMAPINFOHEADER createBitmapHeader(int width, int height)
-{
-    BITMAPINFOHEADER bi;
-
-    // create a bitmap
-    bi.biSize = sizeof(BITMAPINFOHEADER);
-    bi.biWidth = width;
-    bi.biHeight = -height;  //this is the line that makes it draw upside down or not
-    bi.biPlanes = 1;
-    bi.biBitCount = 32;
-    bi.biCompression = BI_RGB;
-    bi.biSizeImage = 0;
-    bi.biXPelsPerMeter = 0;
-    bi.biYPelsPerMeter = 0;
-    bi.biClrUsed = 0;
-    bi.biClrImportant = 0;
-
-    return bi;
-}
-
-cv::Mat captureScreenMat(HWND hwnd)
-{
-    cv::Mat src;
-
-    // get handles to a device context (DC)
-    HDC hWindowDC = GetDC(hwnd);
-    HDC hMemoryDC = CreateCompatibleDC(hWindowDC);
-    SetStretchBltMode(hMemoryDC, COLORONCOLOR);
-
-    // define scale, height and width
-    RECT windowRect;
-    GetClientRect(hwnd, &windowRect);
-    int width = windowRect.right - windowRect.left;
-    int height = windowRect.bottom - windowRect.top;
-
-    // create mat object
-    src.create(height, width, CV_8UC4);
-
-    // create a bitmap
-    HBITMAP hbwindow = CreateCompatibleBitmap(hWindowDC, width, height);
-    BITMAPINFOHEADER bi = createBitmapHeader(width, height);
-
-    // use the previously created device context with the bitmap
-    SelectObject(hMemoryDC, hbwindow);
-
-    // copy from the window device context to the bitmap device context
-    StretchBlt(hMemoryDC, 0, 0, width, height, hWindowDC, 0, 0, width, height, SRCCOPY);    //change SRCCOPY to NOTSRCCOPY for wacky colors !
-    GetDIBits(hMemoryDC, hbwindow, 0, height, src.data, (BITMAPINFO*)&bi, DIB_RGB_COLORS);  //copy from hMemoryDC to hbwindow
-
-    // avoid memory leak
-    DeleteObject(hbwindow);
-    DeleteDC(hMemoryDC);
-    ReleaseDC(hwnd, hWindowDC);
-
-    return src;
 }
