11c250ce0c27044caf981c074cda5861108b9137
KoaLaYT <hytohyeah@outlook.com>
2021-04-17 00:25:49 +0800

    test rewards

--- cpp/test/window/window_test.cpp
+++ cpp/test/window/window_test.cpp
@@ -4,6 +4,10 @@
 
 #include <window/window.hpp>
 #include <imagesearch/screenshot.hpp>
+#include <command/command.hpp>
+
+#define BASE "C:\\Users\\koalayt\\Desktop\\autozhuxian\\assets\\"
+#define PATH(p) BASE p
 
 struct Size {
     int width;
@@ -21,7 +25,7 @@ Size get_rect_size(RECT& rect)
                 rect.bottom - rect.top};
 }
 
-TEST(window, find_window)
+void screenshot_zx(const char* name)
 {
     auto wanmei_platform = autozhuxian::find_window("诛仙3");
     ASSERT_TRUE(wanmei_platform);
@@ -32,5 +36,75 @@ TEST(window, find_window)
 
     cv::Mat screenshot = autozhuxian::screenshot_region(wanmei_platform->handle(),
                                                         autozhuxian::RegionOfInterest{0, 0, win_size.width, win_size.height});
-    cv::imwrite("C:\\Users\\koalayt\\Desktop\\autozhuxian\\assets\\tmp\\screen.png", screenshot);
+    cv::imwrite(name, screenshot);
+}
+
+TEST(window, close_window)
+{
+    // screenshot_zx(PATH("tmp\\screen.png"));
+
+    auto zx = autozhuxian::find_window("诛仙3");
+    ASSERT_TRUE(zx);
+    SetForegroundWindow(zx->handle());
+
+    autozhuxian::ClickByImageCmd cmd{
+        "关闭弹框",
+        autozhuxian::RegionOfInterest::whole,
+        PATH("common\\close_btn.png"),
+        0,
+    };
+    cmd.execute(zx.value());
+
+    // {
+    //     autozhuxian::ClickByPositionCmd cmd{
+    //         "打开奖励界面",
+    //         cv::Point{1418, 115},
+    //         500,
+    //     };
+    //     cmd.execute(zx.value());
+    // }
+
+    // std::vector<autozhuxian::Command*> cmds{
+    //     new autozhuxian::ConfirmImageCmd{
+    //         "确认奖励界面已打开",
+    //         autozhuxian::RegionOfInterest::whole,
+    //         autozhuxian::ImageSearchTargets{
+    //             {PATH("reward\\active.png")},
+    //             {PATH("reward\\levelup.png")},
+    //             {PATH("reward\\signup.png")},
+    //         },
+    //     },
+    //     new autozhuxian::ClickByImageCmd{
+    //         "点击签到奖励",
+    //         autozhuxian::RegionOfInterest::whole,
+    //         autozhuxian::ImageSearchTargets{{PATH("reward\\signup.png")},
+    //                                         {PATH("reward\\signup_selected.png")}},
+
+    //         200,
+    //     },
+    //     new autozhuxian::ClickByImageCmd{
+    //         "领取签到奖励",
+    //         autozhuxian::RegionOfInterest::whole,
+    //         PATH("reward\\signup_confirm.png"),
+    //         200,
+    //     },
+    //     new autozhuxian::ClickByImageCmd{
+    //         "点击升级奖励",
+    //         autozhuxian::RegionOfInterest::whole,
+    //         autozhuxian::ImageSearchTargets{{PATH("reward\\levelup.png")},
+    //                                         {PATH("reward\\levelup_selected.png")}},
+
+    //         200,
+    //     },
+    //     new autozhuxian::ClickByImageCmd{
+    //         "领取升级奖励",
+    //         autozhuxian::RegionOfInterest::whole,
+    //         PATH("reward\\levelup_confirm.png"),
+    //         200,
+    //     },
+    // };
+
+    // for (auto& cmd : cmds) {
+    //     cmd->execute(zx.value());
+    // }
 }
