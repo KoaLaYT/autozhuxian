98c391654b723672f7eaec951c77baf79f4a68a9
KoaLaYT <hytohyeah@outlook.com>
2021-04-19 14:07:39 +0800

    refactor lib/control comments

--- cpp/include/control/control.hpp
+++ cpp/include/control/control.hpp
@@ -7,33 +7,32 @@
 
 namespace autozhuxian {
 
-/**
- * @brief 模拟按键
- * 
- * @param keycode 
- */
+///
+/// 模拟按键
+/// ---------------------------------------------------------
+/// keycode对应表：https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
+///
 void press(DWORD keycode);
 
-/**
- * @brief 鼠标移动（相对屏幕的绝对位置）
- * 
- * @param x 
- * @param y 
- */
+///
+/// 鼠标移动（相对屏幕的绝对位置）
+/// ---------------------------------------------------------
+///
+///
 void move(int x, int y);
 
-/**
- * @brief 鼠标点击当前位置
- * 
- */
+///
+/// 鼠标点击当前位置
+/// ---------------------------------------------------------
+///
+///
 void click();
 
-/**
- * @brief 鼠标点击相对屏幕的绝对位置
- * 
- * @param x 
- * @param y 
- */
+///
+/// 鼠标点击相对屏幕的绝对位置
+/// ---------------------------------------------------------
+///
+///
 void click(int x, int y);
 
 };  // namespace autozhuxian
--- cpp/lib/control/control.cpp
+++ cpp/lib/control/control.cpp
@@ -5,6 +5,11 @@
 
 namespace impl {
 
+///
+/// 获取屏幕的尺寸
+/// ---------------------------------------------------------
+///
+///
 static std::pair<int, int> get_desktop_size()
 {
     HWND desktop = GetDesktopWindow();
@@ -18,10 +23,15 @@ static std::pair<int, int> get_desktop_size()
 
 namespace autozhuxian {
 
+///
+/// 控制
+/// ---------------------------------------------------------
+/// 模拟按键
+///
 void press(DWORD keycode)
 {
     std::array<INPUT, 2> inputs;
-    inputs.fill({});
+    inputs.fill({});  //! IMPORTANT，详见docs/zero_initialize.md
 
     inputs[0].type = INPUT_KEYBOARD;
     inputs[0].ki.wVk = keycode;
@@ -34,22 +44,33 @@ void press(DWORD keycode)
     SendInput(inputs.size(), inputs.data(), sizeof(INPUT));
 }
 
+///
+/// 控制
+/// ---------------------------------------------------------
+/// 鼠标移动（相对屏幕的绝对位置）
+///
 void move(int x, int y)
 {
     auto size = impl::get_desktop_size();
 
-    INPUT input{};
+    INPUT input{};  //! IMPORTANT，详见docs/zero_initialize.md
     input.type = INPUT_MOUSE;
+    // 根据msdn文档，需要作这样的转换
     input.mi.dx = (double)x / size.first * 65536;
     input.mi.dy = (double)y / size.second * 65536;
     input.mi.dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE;
     SendInput(1, &input, sizeof(INPUT));
 }
 
+///
+/// 控制
+/// ---------------------------------------------------------
+/// 鼠标点击当前位置
+///
 void click()
 {
     std::array<INPUT, 2> inputs;
-    inputs.fill({});
+    inputs.fill({});  //! IMPORTANT，详见docs/zero_initialize.md
 
     inputs[0].type = INPUT_MOUSE;
     inputs[0].mi.dx = 0;
@@ -64,6 +85,11 @@ void click()
     SendInput(inputs.size(), inputs.data(), sizeof(INPUT));
 }
 
+///
+/// 控制
+/// ---------------------------------------------------------
+/// 鼠标点击相对屏幕的绝对位置
+///
 void click(int x, int y)
 {
     move(x, y);
